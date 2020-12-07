7ea02343bb091bd3b2e630f4dcb3c90162a0f87c
Ashlee <36325294+cannonashlee@users.noreply.github.com>
2020-12-07 13:16:04 -0800

    company link change

--- script.js
+++
@@ -1,7 +0,0 @@
-function openNewPage() {
-    if(document.getElementById('maintitle').value){
-      var href = document.getElementById('maintitle').value
-      window.open(href,'#').value;
-    }   
-  }
-
