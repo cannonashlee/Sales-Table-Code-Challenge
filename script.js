cadbccedfbdf9d401dd1b4bb6aa77f697287ecd7
Ashlee <36325294+cannonashlee@users.noreply.github.com>
2020-12-09 14:39:58 -0800

    deleted test comments

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// let quarter1 = document.querySelector("input [name=allcheckbox]");
-// let quarter2 = document.querySelector('#q2').checked;
-// let quarter3 = document.querySelector('#q3').checked; 
-// let quarter4 = document.querySelector('#q4').checked; 
-
-// console.log("quarter 2 is checked", quarter2); 
-// console.log("quarter 3 is checked", quarter3);
-// console.log("quarter 4 is checked", quarter4); 
-
 const checkbox = document.querySelector('#q1');
 const checkbox2 = document.querySelector('#q2');
 const checkbox3 = document.querySelector('#q3');
